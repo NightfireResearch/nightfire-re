3e98900a121480af82cfb7ae0d6807fa2636ff03
Charlie Bruce <charliebruce@gmail.com>
2023-09-04 23:38:20 +0100

    Deinterleave and extract music

--- extract_audio.py
+++ extract_audio.py
@@ -1,7 +1,7 @@
 # Extract audio out of banks into separate files
 # then decode into .wav or some other modern format
 import struct
-
+import util
 
 # Step 1: Extract the banks
 
@@ -14,82 +14,170 @@ import struct
 # This appears to be incomplete - the .ELF file mentions a number of extra entries, eg SFX_WEAPON_XBOW_SHOT_01
 # However this debug does tell us a bit about the directory structure/original file format.
 
-# Start with PS2/ENGLISH/SB_0/SB_0
+
+def extract(bank, subbank):
+
+	path = f"extract/PS2/ENGLISH/SB_{bank}/SB_{subbank}"
+
+	with open(f"{path}.SBF", "rb") as f:
+		sbf = f.read()
+
+	with open(f"{path}.SFX", "rb") as f:
+		sfx = f.read()
+
+	with open(f"{path}.SHF", "rb") as f:
+		shf = f.read()
+
+	# Data in SBF (Megabytes)
+	# ?? in SFX (692 bytes)
+	# ?? in SHF (220 bytes)
 
 
+	print(f"Data bank size {len(sbf)}")
+
+	# SHF
+	p_shf = 0
+	numShf = struct.unpack("<I", shf[p_shf:p_shf+4])[0]
+	p_shf += 4
+
+	print(f"Got numShf: {numShf}")
+
+	toc = []
+	for i in range(numShf):
+		loop, offset, size, freq, unk1, ch, bits, unk2, unk3 = struct.unpack("<IIIIIIIII", shf[p_shf:p_shf+36])
+		p_shf += 36
+
+		#freq = freq * 12
+		# Frequency seems APPROX right but some error here?
+
+		# Unclear if/why this is needed vs just *12
+		# FIXME some end up breaking ffmpeg
+		f_real = {2731:32000, 1621:20000, 1882:24000, 941:11025, 1024:12000, 683:8000}[freq]
+
+		# Works for the final 2 tracks
+		#freq=32000
+
+		toc.append((loop, offset, size, freq, ch, bits,))
+
+		print(f"Got track with freq {freq}, channels {ch}, bits {bits}, loop {loop}")
+
+		# Take the defined slice from the SBF data, attach a header
+		with open(f"audio/bnk{bank}_{subbank}_{i}.vag", "wb") as of:
+			# SShd header
+			#header = '\x53\x53\x68\x64\x18\x00\x00\x00\x10\x00\x00\x00\xb0\x36\x00\x00\x02\x00\x00\x00\xc0\x2f\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\x53\x53\x62\x64\x00\xe8\x11\x00'
+			
+			# VAG header
+			of.write("VAGp".encode("ascii"))
+			of.write(struct.pack("<I", 0x20)) # Version
+			of.write(struct.pack("<I", 0x00)) # Reserved
+			of.write(struct.pack("<I", 0x01)) # Channel size? - 1, 4, 0?
+			of.write(struct.pack(">I", f_real)) # Sample rate, Hz - weirdly LE not BE? Or is this FFMpeg bug?
+			of.write(bytes([0]*12)) # Reserved
+			of.write("TESTFILE".encode("ascii"))
+			of.write(bytes([0]*8)) # Padding
+			of.write(sbf[offset:offset+size]) # PSX ADPCM data
 
 
-with open("extract/PS2/ENGLISH/SB_0/SB_2.SBF", "rb") as f:
-	sbf = f.read()
+	# SFX - is this how we get stereo vs mono? (num SFX is <= nu SHF)
+	p_sfx = 0
+	numSfx = struct.unpack("<I", sfx[p_sfx:p_sfx+4])[0]
+	p_sfx += 4
 
-with open("extract/PS2/ENGLISH/SB_0/SB_2.SFX", "rb") as f:
-	sfx = f.read()
+	print(f"Got numSfx: {numSfx}")
 
-with open("extract/PS2/ENGLISH/SB_0/SB_2.SHF", "rb") as f:
-	shf = f.read()
+	for i in range(numSfx):
+		unk1, unk2 = struct.unpack("<II", sfx[p_sfx:p_sfx+8])
+		p_sfx += 8
 
-# Data in SBF (Megabytes)
-# ?? in SFX (692 bytes)
-# ?? in SHF (220 bytes)
+		print(f"SFX Data is {unk1}, {unk2}")
+
+	# Step 2: Convert to a useful format, combining stereo channels if needed?
+	# ffmpeg -i test_100.vag test_100.wav
 
 
-print(f"Data bank size {len(sbf)}")
+# extract(0, 0)
+# extract(0, 1)
+# extract(0, 2)
+# extract(0, 3)
+# extract(0, 4)
+# extract(0, 5)
+# extract(0, 6)
+# extract(0, 7)
+# extract(1, 8)
+# extract(1, 9)
+# extract(1, 10)
+# extract(1, 11)
+# extract(1, 12)
+# extract(1, 13)
+# extract(1, 14)
+# extract(1, 15)
+# extract(2, 16)
+# extract(2, 17)
+# extract(2, 18)
+# extract(2, 19)
+# extract(2, 20)
+# extract(2, 21)
+# extract(2, 22)
+# extract(2, 23)
+# extract(3, 24)
 
-# SHF
-p_shf = 0
-numShf = struct.unpack("<I", shf[p_shf:p_shf+4])[0]
-p_shf += 4
 
-print(f"Got numShf: {numShf}")
+# Sounds weird - interleaved stereo?
+def music_extract(bank, subbank):
 
-toc = []
-for i in range(numShf):
-	loop, offset, size, freq, unk1, ch, bits, unk2, unk3 = struct.unpack("<IIIIIIIII", shf[p_shf:p_shf+36])
-	p_shf += 36
+	path = f"extract/PS2/MUSIC/MFX_{bank}/MFX_{subbank}"
 
-	#freq = freq * 12
-	# Frequency seems APPROX right but some error here?
+	# SMF (meta file) and SSD (data)	
+	with open(f"{path}.SMF", "rb") as f:
+		smf = f.read()
 
-	# Unclear if/why this is needed vs just *12
-	# FIXME some end up breaking ffmpeg
-	f_real = {2731:32000, 1621:20000, 1882:24000, 941:12000, 1024:12000}[freq]
+	with open(f"{path}.SSD", "rb") as f:
+		ssd = f.read()
 
-	# Works for the final 2 tracks
-	#freq=32000
+	p_smf = 0
+	numSmf = struct.unpack("<I", smf[p_smf:p_smf+4])[0]
+	p_smf += 4
 
-	toc.append((loop, offset, size, freq, ch, bits,))
+	print(f"Music bank {bank}-{subbank} has {numSmf} tracks")
 
-	print(f"Got track with freq {freq}, channels {ch}, bits {bits}, loop {loop}")
+	# Data is 2-channel, 16-bit samples, 128-byte (0x80) interleave, 32000Hz rate
+	channel_l = b''.join(list(util.chunks(ssd, 128))[::2])
+	channel_r = b''.join(list(util.chunks(ssd, 128))[1::2])
+	deinterleaved={"l":channel_l, "r":channel_r}
 
-	# Take the defined slice from the SBF data, attach a header
-	with open(f"test_{i}.vag", "wb") as of:
-		# SShd header
-		#header = '\x53\x53\x68\x64\x18\x00\x00\x00\x10\x00\x00\x00\xb0\x36\x00\x00\x02\x00\x00\x00\xc0\x2f\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\x53\x53\x62\x64\x00\xe8\x11\x00'
-		
-		# VAG header
+	for ch in ["l","r"]:
+		with open(f"audio/mus_{bank}_{subbank}_{ch}.vag", "wb") as of:
+			of.write("VAGp".encode("ascii"))
+			of.write(struct.pack("<I", 0x20)) # Version
+			of.write(struct.pack("<I", 0x00)) # Reserved
+			of.write(struct.pack("<I", 0x01)) # Channel size? - 1, 4, 0? FFMPEG does not pick this up, just guesses?
+			of.write(struct.pack(">I", 32000)) # Sample rate, Hz - weirdly LE not BE? Or is this FFMpeg bug?
+			of.write(bytes([0]*12)) # Reserved
+			of.write("TESTFILE".encode("ascii"))
+			of.write(bytes([0]*8)) # Padding
+
+			of.write(deinterleaved[ch]) # PSX ADPCM data
+
+
+# TODO: MFXINFO file tells us the number of banks?
+for i in range(1, 16):
+	music_extract(0, i)
+music_extract(1, 16)
+
+
+def extract_streams():
+	# Contains spoken voice lines
+	# TODO: LUT parsing
+	with open("extract/PS2/ENGLISH/STREAMS/STREAMS.BIN", "rb") as f:
+		streams = f.read()
+
+	with open("audio/streams.vag", "wb") as of:
 		of.write("VAGp".encode("ascii"))
 		of.write(struct.pack("<I", 0x20)) # Version
 		of.write(struct.pack("<I", 0x00)) # Reserved
 		of.write(struct.pack("<I", 0x01)) # Channel size? - 1, 4, 0?
-		of.write(struct.pack(">I", f_real)) # Sample rate, Hz - weirdly LE not BE? Or is this FFMpeg bug?
+		of.write(struct.pack(">I", 22050)) # Sample rate, Hz - weirdly LE not BE? Or is this FFMpeg bug?
 		of.write(bytes([0]*12)) # Reserved
 		of.write("TESTFILE".encode("ascii"))
 		of.write(bytes([0]*8)) # Padding
-		of.write(sbf[offset:offset+size]) # PSX ADPCM data
-
-
-# SFX
-p_sfx = 0
-numSfx = struct.unpack("<I", sfx[p_sfx:p_sfx+4])[0]
-p_sfx += 4
-
-print(f"Got numSfx: {numSfx}")
-
-for i in range(numSfx):
-	unk1, unk2 = struct.unpack("<II", sfx[p_sfx:p_sfx+8])
-	p_sfx += 8
-
-	print(f"SFX Data is {unk1}, {unk2}")
-
-# Step 2: Convert to a useful format
-# ffmpeg -i test_100.vag test_100.wav
+		of.write(streams) # PSX ADPCM data
--- extract_bin.py
+++ extract_bin.py
@@ -94,7 +94,7 @@ def extract_all(target_dir):
 			# 0x0B: Change memory type to 1?
 			# 0x0C: Some weird behaviour that modifies LoadableIndex?? Some kind of deferred loading? Triggerable by scripting?
 			# 0x0f: Icon (only used by PS2 memory card?)
-			# 0x10: Woman (only used by menu animation?)
+			# 0x10: Woman (handled by decompress_woman.py)
 
 			# For the next tool in the pipeline, let's give each file an extension based on its attrs
 			hh = "{:02x}".format(attrs)
