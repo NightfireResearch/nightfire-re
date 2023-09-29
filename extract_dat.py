fc119d37b24128c265f4617f113d6e8d849622ec
Charlie Bruce <charliebruce@gmail.com>
2023-09-29 22:20:57 +0100

    Fix translations export

--- extract_dat.py
+++ extract_dat.py
@@ -1,11 +1,5 @@
 # .DAT files contain language specific data (translations). There's the "U" and non-"U" form (UTF16-encoded Unicode?)
-# After the data is the table of offsets (?)
-# How do we know where the table starts?
 
-# DU: around 1C440 (first bytes are 30 C4 01 00)
-# FR: around 1BFA0 (first bytes are 88 BF 01 00)
-# UK: around 19020 (first bytes are 0B 90 01 00)
-# So first 4 bytes just gives the offset of the table
 import os
 import struct
 import util
@@ -15,17 +9,31 @@ def extract_all(target_dir):
 		if not filename.endswith(".dat"):
 			continue
 
+		is_unicode = ("txtu.dat" in filename.lower())
+		lang_id = filename.lower().split("txt")[0]
+
 		with open(target_dir + "/" + filename, "rb") as f:
 			text_data = f.read()
 
-		table_offset = (struct.unpack("<I", text_data[0:4])[0] + 7) & ~0b11 # the table seems to be slightly off - a header + padding to next word?
+		lut_offset = struct.unpack("<I", text_data[0:4])[0] + 4 # Offset relative to this u32, NOT relative to the file start.
 
+		## Let's understand the structure:
+		# USATxtU: Starts 0x31ED4 (204500). At (205400+4), we see 17171717F10A0000 then the offset table
+		# USATxt:  Starts 0x18F6A (102250). At (102250+4), we see 1717F10A0000 then the offset table
+		# FRTxtU:  Starts 0x37F10 (229136). At (229136+4), we see 17171717F10A0000 then the offset table
+		# 0x17 is Ascii "End Table Block". Used for padding?
+		# 0x0AF1 is 2801 - the number of strings we expect.
 
-		table_len = struct.unpack("<I", text_data[table_offset:table_offset+4])[0]
-		table_data = text_data[table_offset+4:]
+		# Consume padding. Note that if the last string happens to align on a word boundary already, we must add 4 bytes of padding, not zero!
+		if(lut_offset % 4) != 0:
+			lut_offset = 4 * ((lut_offset + 3) // 4)
+		else:
+			lut_offset += 4
 
+		table_len = struct.unpack("<I", text_data[lut_offset:lut_offset+4])[0]
+		table_data = text_data[lut_offset+4:]
 
-		print(f"Translation file {filename} - has {table_len} entries")
+		print(f"Translation file {filename} is language {lang_id}, unicode: {is_unicode} has {table_len} entries")
 
 		entries = list(util.chunks(table_data, 4))[0:table_len] # padding at end?
 
@@ -33,6 +41,11 @@ def extract_all(target_dir):
 			str_offset = struct.unpack("<I", e)[0] + 4
 
 			# FIXME decoding 0x99 fails. This looks like a special character they might have custom-allocated for (C) or (R) symbol?
+			# Fixme >1000 char length - read to next
 			s = text_data[str_offset: str_offset + 100].decode("utf-8", 'replace').split("\x00")[0]
 
 			#print(f"Got string: {s}")
+
+
+if __name__=="__main__":
+	extract_all("files_bin_unpack")
