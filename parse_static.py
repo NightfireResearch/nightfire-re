8458e1fcc63de2c5aef731c817737d214e7ccd4b
Charlie Bruce <charliebruce@gmail.com>
2023-12-02 19:31:31 +0000

    WIP on placements

--- parse_static.py
+++ parse_static.py
@@ -38,10 +38,13 @@ def toBlocks(data):
         rotation = struct.unpack("<fff", data[offset+24:offset+36])
 
         unk1 = struct.unpack("<12B", data[offset+36:offset+48])
+
+        # Often but not always around 1
         unk2 = struct.unpack("<ffff", data[offset+48:offset+64])
+
         unk3 = struct.unpack("<8B", data[offset+64:offset+72])
 
-        numExtraData, = struct.unpack("<I", data[offset+72:offset+76]) # At 0x4c
+        numExtraData, = struct.unpack("<I", data[offset+72:offset+76])
         # Extra data is given in units of 8 bytes
         numExtraBytes = 8 * numExtraData
 
@@ -56,6 +59,12 @@ def toBlocks(data):
             'extraData': extraData,
         }
 
+        # A block should be identified either with a hashcode (index: 0xFFFF) or an index (hashcode=0xFFFFFFFF)
+        assert (gfxHashcode != 0xFFFFFFFF or index != 0xFFFF), "Both index and hashcode are invalid identifiers"
+
+        # In some cases a block can both have an index and a hashcode - not sure what to do here?
+        #, assert not (gfxHashcode != 0xFFFFFFFF and index != 0xFFFF), f"Two conflicting identifiers (index: {index}, hashcode: {gfxHashcode:08x})"
+
         print(f"Placement of {gfxHashcode:08x} / index {index} at ({transform[0]}, {transform[1]}, {transform[2]}), extra data: {numExtraBytes} bytes")
 
         blocks.append(block)
@@ -69,7 +78,9 @@ def toBlocks(data):
 
 if __name__=="__main__":
 
-    filename = "level_unpack/0700000a_HT_Level_TowerB/_staticdata_41.bin"
+    #filename = "level_unpack/0700000a_HT_Level_TowerB/_staticdata_41.bin"
+    filename = "level_unpack/0700000a_HT_Level_TowerB/_staticdata_2351.bin" # The big one containing most level fragments?
+
     print(f"Loading blocks from static placement file {filename}")
 
     with open(filename, "rb") as f:
